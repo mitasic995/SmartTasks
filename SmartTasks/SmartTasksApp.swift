d8eafbaa1bba9affddec11acdfbdfc1c748736e7
Miloš Tasić <tasic_milos@yahoo.com>
2025-10-05 20:27:16 +0200

    Initial Commit

---
+++ SmartTasks/SmartTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartTasksApp.swift
+//  SmartTasks
+//
+//  Created by Milos Tasic on 5. 10. 2025..
+//
+
+import SwiftUI
+
+@main
+struct SmartTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
