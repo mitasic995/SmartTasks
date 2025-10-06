1c5d049b3e9a2d03d0db63c49400da721f149a17
Miloš Tasić <tasic_milos@yahoo.com>
2025-10-06 16:51:46 +0200

    Add SmartTasks's colors.

---
+++ SmartTasks/Extensions/Color+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extensions.swift
+//  SmartTasks
+//
+//  Created by Milos Tasic on 6. 10. 2025..
+//
+
+import Foundation
+import SwiftUICore
+
+extension Color {
+    static let smartTasksGreen = Color("Green")
+    static let smartTasksYellow = Color("Yellow")
+    static let smartTasksBeige = Color("Beige")
+    static let smartTasksRed = Color("Red")
+}
