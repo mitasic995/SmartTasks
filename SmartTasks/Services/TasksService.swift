a867333e4a6aba2fba0afb6d32f76e20053dc4cc
Miloš Tasić <tasic_milos@yahoo.com>
2025-10-06 13:23:51 +0200

    Add `TasksServiceError`.

--- SmartTasks/Services/TasksService.swift
+++ SmartTasks/Services/TasksService.swift
@@ -11,6 +11,10 @@ private enum Constants {
     static let getTasksRequestUrl = "http://demo9877360.mockable.io/"
 }
 
+enum TasksServiceError: Error {
+    case failedToDecodeResponse
+}
+
 class TasksService {
     private let decoder: JSONDecoder
     private let httpClient: Networking
@@ -26,14 +30,20 @@ class TasksService {
 
 extension TasksService: TasksProviding {
     func tasks() async throws -> [Task] {
-        guard let url = URL(string: Constants.getTasksRequestUrl) else { throw URLError(.badURL) }
-        
-        let request = URLRequest(url: url)
-        
-        let data = try await httpClient.send(request)
-        
-        let dto = try decoder.decode(Tasks.self, from: data)
-        
-        return dto.tasks
+        do {
+            guard let url = URL(string: Constants.getTasksRequestUrl) else { throw URLError(.badURL) }
+            
+            let request = URLRequest(url: url)
+            
+            let data = try await httpClient.send(request)
+            
+            let dto = try decoder.decode(Tasks.self, from: data)
+            
+            return dto.tasks
+        } catch is DecodingError {
+            throw TasksServiceError.failedToDecodeResponse
+        } catch {
+            throw error
+        }
     }
 }
