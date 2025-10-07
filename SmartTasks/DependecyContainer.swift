9f99436faa9b14eb0a23fb3bc09fbb6fba170dee
Miloš Tasić <tasic_milos@yahoo.com>
2025-10-07 21:27:15 +0200

    Add `DependencyContainter`.

---
+++ SmartTasks/DependecyContainer.swift
@@ -0,0 +1,12 @@
+//
+//  DependecyContainer.swift
+//  SmartTasks
+//
+//  Created by Milos Tasic on 7. 10. 2025..
+//
+
+import Foundation
+
+final class DependencyContainer {
+    let tasksService = TasksService(httpClient: URLSession.shared)
+}
